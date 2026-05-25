32b628b250a8d0460dd0d732578237233205454d
TARS-90 <grzywiastyyy@gmail.com>
2026-05-26 01:54:32 +0200

    Refactoring client.c functions

--- client/src/client.c
+++ client/src/client.c
@@ -4,49 +4,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <string.h>
 #include <time.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <SDL3/SDL.h>
 
-void print_game(GameState *gs) {
-    system("clear");
-    for (int row = 0; row < HEIGHT; row++) {
-        for (int col = 0; col < WIDTH; col++) {
-            int index = (col * HEIGHT) + row;
-            int player_drawn = 0;
-
-            for (int i = 0; i < MAX_PLAYERS; i++) {
-                if (gs->players[i].x == col && gs->players[i].y == row && gs->players[i].x != -1) {
-                    printf("%d", gs->players[i].color);
-                    player_drawn = 1;
-                    break; 
-                }
-            }
-
-            if (!player_drawn) {
-                if (gs->board[index] == OBJECT_EMPTY)    printf("_");
-                else if (gs->board[index] == OBJECT_WALL) printf("X");
-            }
-        }
-        printf("\n");
-    }
-}
+typedef struct ThreadData {
+	int *sock_fd;
+	GameState *game;
+} ThreadData;
 
 void *receive_game_state(void* args) {
-	int *sock_fd = (int*) args;
-	GameState game_state;
+	ThreadData *data = (ThreadData*) args;
 
 	while (1) {
-		if (recv(*sock_fd, &game_state, sizeof(GameState), 0) <= 0) {
+		if (recv(*data->sock_fd, data->game, sizeof(GameState), 0) <= 0) {
 		    printf("Połączenie z serwerem przerwane.\n");
 		    break;
 		}		
-		print_game(&game_state);
 	}
 }
-
 
 int connect_to_server(int *sock_fd, struct sockaddr_in *server_addr) {
 	if ((*sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -54,13 +30,13 @@ int connect_to_server(int *sock_fd, struct sockaddr_in *server_addr) {
 		return -1;
 	}
 
-	*server_addr = {
+	*server_addr = (struct sockaddr_in) {
 		.sin_family = AF_INET,
 		.sin_port = htons(12345),
 		.sin_addr.s_addr = inet_addr("127.0.0.1")
 	};
 
-	if (connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+	if (connect(*sock_fd, (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0) {
 		perror("Connecting with server failed!\n");
 		return -1;
 	}
@@ -68,43 +44,47 @@ int connect_to_server(int *sock_fd, struct sockaddr_in *server_addr) {
 	return 0;
 }
 
+
 void run_client() {
 	int sock_fd;
 	struct sockaddr_in server_addr;
 	if (connect_to_server(&sock_fd, &server_addr) == -1) return;
-	
+
+	// waiting for protocol from server that starts the game
+	MessageType msg;
+	if (recv(sock_fd, &msg, sizeof(MessageType), 0) <= 0 || msg != MSG_START_GAME) {
+		perror("Start game failed!\n");
+		close(sock_fd);
+		return;
+	}
+
+	// game has started
+
+	GameState game_state;
+	ThreadData thread_data = (ThreadData) {
+		.sock_fd = &sock_fd,
+		.game = &game_state
+	};
 	pthread_t pid;
-	if (pthread_create(&pid, receive_game_state, (void*) sock_fd) != 0) {
+	if (pthread_create(&pid, NULL, receive_game_state, (void*) &thread_data) != 0) {
 		perror("Creating thread for receiving game state failed!\n");
 		close(sock_fd);
 		return;
 	}
-    
 
-	// main loop 
-	while (1) {
-		printf("\nPodaj zadanie (w/a/s/d/p): ");
-		fflush(stdout); 
-		
-		char c;
-		if (scanf(" %c", &c) != 1) continue; 
-		
-		MessageType msg = MSG_NONE;
-		switch (c) {
-		    case 'w': msg = MSG_MOVE_UP;    break;
-		    case 's': msg = MSG_MOVE_DOWN;  break;
-		    case 'd': msg = MSG_MOVE_RIGHT; break;
-		    case 'a': msg = MSG_MOVE_LEFT;  break;
-		    case 'p': msg = MSG_PLACE_BOMB; break;
-		    default:  msg = MSG_NONE;       break;
-		}
-		
-		if (msg != MSG_NONE) {
-		    send(sock_fd, &msg, sizeof(msg), 0);
-		}
-		
-		usleep(FRAME_DURATION); // 50ms
-	    }
+	if (sdl_engine_init() == -1) {
+		close(sock_fd);
+		return;
+	}
 
-    close(sock_fd);
+	// main loop
+	while (!game_state.is_end) {
+		sdl_engine_render(&game_state);
+		usleep(FRAME_DURATION);
+	}
+
+	// releasing resources
+	sdl_engine_shutdown();
+	pthread_join(pid, NULL);
+	close(sock_fd);
 }
